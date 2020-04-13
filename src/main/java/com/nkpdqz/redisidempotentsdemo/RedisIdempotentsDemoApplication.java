6111c9798e9a14b38906eceea78908138f212396
Nkpdqz <529446386@qq.com>
2020-04-13 17:33:06 +0800

    Initial commit

---
+++ src/main/java/com/nkpdqz/redisidempotentsdemo/RedisIdempotentsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nkpdqz.redisidempotentsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisIdempotentsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisIdempotentsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nkpdqz/redisidempotentsdemo/RedisIdempotentsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkpdqz.redisidempotentsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisIdempotentsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
